08672cd39364b341deb5a947c0ed8a865aeba831
Your Name <you@example.com>
2020-12-24 23:33:07 +0530

    Added: All validations for every attribute mentioned

--- src/main/java/com/bridgelabz/addressBook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressBook/dto/AddressBookDTO.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressBook.dto;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.ToString;
@@ -15,9 +16,12 @@ public @ToString class AddressBookDTO {
 	@Pattern(regexp = "^91{2}[ ]{1}[789]{9}$", message = "Phone Number Invalid")
 	public long phoneNumber;
 	
+	@NotNull(message = "City should not be Empty")
 	public String City;
 	
+	@NotNull(message = "State should not be Empty")
 	public String State;
 	
+	@NotNull(message = "ZipCode should not be Empty")
 	public long ZipCode;
 }
