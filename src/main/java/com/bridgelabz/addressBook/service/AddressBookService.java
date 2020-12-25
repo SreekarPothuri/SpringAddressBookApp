0a35a44670b708a00aad5cedac0db672b9a5cdc6
Your Name <you@example.com>
2020-12-25 17:31:57 +0530

    Added: code to save data to database using Jpa-hibernate methods

--- src/main/java/com/bridgelabz/addressBook/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressBook/service/AddressBookService.java
@@ -3,15 +3,23 @@ package com.bridgelabz.addressBook.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.addressBook.dto.AddressBookDTO;
 import com.bridgelabz.addressBook.exception.AddressBookException;
 import com.bridgelabz.addressBook.model.AddressBookData;
+import com.bridgelabz.addressBook.repository.AddressbookRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class AddressBookService implements IAddressBookService{
 
+	@Autowired
+	private AddressbookRepository addressBookRepository;
+	
 	private List<AddressBookData> contactDataList = new ArrayList<>();
 	public List<AddressBookData> getAddressBookData() {
 		return contactDataList;
@@ -26,9 +34,10 @@ public class AddressBookService implements IAddressBookService{
 
 	public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
 		AddressBookData contactData = null;
-		contactData = new AddressBookData(contactDataList.size()+1, addressBookDTO);
+		contactData = new AddressBookData(addressBookDTO);
 		contactDataList.add(contactData);
-		return contactData;
+		log.debug("Contact Data: "+contactData.toString());
+		return addressBookRepository.save(contactData);
 	}
 
 	public AddressBookData updateAddressBookData(int bookId, AddressBookDTO addressBookDTO) {
